7faf593b0174d8cbaf187e0fb597e421e755cec5
Samuel Korantng Amoah Bampo <sambampo200@gmail.com>
2023-02-16 13:06:56 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <studio.h>
+/**
+ /*main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+}
+        puts("\"Programming is like building a multilingual puzzile");
+        return (0);
+
+}
